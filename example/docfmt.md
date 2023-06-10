99fd2cd001fd5ca72e5b6fc7ca0455827012d6d0
dilobaririsova <dilobaririsova@dilobars-mbp.home>
2023-06-09 23:31:00 -0400

    1

---
+++ example/docfmt.md
@@ -0,0 +1,24 @@
+General:
+
+%v (value in default format)
+%T (type)
+%% (literal %)
+
+
+Boolean
+%t (true or false)
+
+Integer
+
+%b (base 2)   this is actually the binary representation ex: fmt.Printf("Number: %b\n", 1111)
+%o (base 8)   octal 
+%d (base 10)  decimal give us the same value
+%x (base 16)   hexadecimal also we can use %X 
+
+Floating Points
+%e (scientific notation)
+%f / %F (decimal no exponent)
+
+
+
+
---
+++ example/hello.go
@@ -0,0 +1,100 @@
+package main 
+import "fmt"
+
+
+// func main() {
+// 	name := "John"
+// 	age := 30
+// 	fmt.Println("Name: %s, Age: %d\n", name, age)
+// }
+
+//Variables
+// func main() {
+// 	// var x int = 5
+// 	// var y int = 6
+// 	// var sum int = x+y
+// 	// or we can declare variable 
+// 	x := 5
+// 	y := 6
+// 	sum := x+y
+// 	fmt.Println(sum)
+// }
+
+//If statements
+// func main() {
+// 	x := 2
+// 	if x > 6 {
+// 		fmt.Println("More than 6")
+// 	} else if x < 2 {
+// 		fmt.Println("Less than 2")
+// 	} else {
+// 		fmt.Println("Hello")
+// 	}
+// }
+
+//Arrays and slices
+// func 
+// 	var pipa [5]int //  [0 0 0 0 0]
+// 	a[2] = 7 // output [0 0 7 0 0]
+// //or
+// 	b := [5]int{5, 4, 3, 2, 1} //output [5 4 3 2 1]
+// //slices
+	// c := []int{5, 4, 3, 2, 1, 0}
+//append
+// 	d := []int{1, 2, 3}
+// 	d = append(d, 13)
+
+// 	fmt.Println(d)
+// }
+
+// Maps is like a dictionary in python 
+// func main() {
+// 	vertices := make(map[string]int)
+// 	vertices["triangle"] = 2
+// 	vertices["square"] = 3
+// 	vertices["dodecagon"] = 12
+	
+// 	fmt.Println(vertices)
+	  
+// }
+
+
+// Variable & Data Types
+// func main() {
+	// var name string = "Dilobar"
+	// name = "John"  //change the var value
+	// name = "Bob"  //change the var again
+// numbers
+	// var number uint16 = 260 
+	// number += 6  // this is equel to the number = number + 6
+	// fmt.Println("Total:", number)
+// }
+
+// """Assignment Expression & Implicit vs Explicit"""
+// int age;
+// String name;
+// double salary;  """This is Explicit variable declaration"""
+
+// age = 25
+// name = "John Doe"
+// salary = 5000.00 """This is Implicit variable declaration"""
+
+// func main() {
+// 	number := 20000.23
+// 	fmt.Printf("%T", number)
+// }
+
+// Printing to Console & fmt
+// func main() {
+// 	fmt.Printf("HELLO %T %v \n", 10, 10)  //%T: This placeholder is replaced by the type of the value 10, which is int
+// }										//%v: This placeholder is replaced by the value 10 itself.
+
+
+// Integer
+func main() {
+	fmt.Printf("Number: %o\n", 1111)
+}
+
+
+
+
